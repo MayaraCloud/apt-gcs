b16eb9190367031eef8bbbeba38406ad892b53f5
Dario Ferrer <dario@mayara.io>
2020-02-26 19:52:10 +0100

    Access_token: gcp access_token auth support added

--- init.go
+++ init.go
@@ -1,14 +1,18 @@
 package gcs
 
 import (
+	"net/http"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
+	"golang.org/x/oauth2"
+	"io/ioutil"
 	storage "google.golang.org/api/storage/v1"
 	"log"
 )
 
 const (
 	scope = storage.DevstorageReadOnlyScope
+	accessTokenPath = "/etc/apt/gcs_access_token"
 )
 
 var (
@@ -17,13 +21,18 @@ var (
 	oService *storage.ObjectsService
 )
 
+var ctx context.Context = context.Background()
+
+//InitConfig creates the google storage client from the
+//application default credentials or from an access_token
 func InitConfig() {
-
-	client, err := google.DefaultClient(context.Background(), scope)
+	client, err := google.DefaultClient(ctx, scope)
 	if err != nil {
-		log.Fatalf("Unable to get default client: %v", err)
+		client, err = clientFromAccessToken(accessTokenPath)
+		if err != nil {
+			log.Fatalf("Unable to get client: %v", err)
+		}
 	}
-
 	service, err = storage.New(client)
 	if err != nil {
 		log.Fatalf("Unable to create storage service: %v", err)
@@ -34,4 +43,13 @@ func InitConfig() {
 		log.Fatalf("Unable to create objects storage service: %v", err)
 	}
 
+}
+
+func clientFromAccessToken(accessTokenPath string) (client *http.Client, err error) {
+	tokenBytes, err := ioutil.ReadFile(accessTokenPath)
+	token := oauth2.Token{
+		AccessToken: string(tokenBytes),
+	}
+	tokenSource := oauth2.StaticTokenSource(&token)
+	return oauth2.NewClient(ctx, tokenSource), err
 }
